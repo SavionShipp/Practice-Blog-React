f4b901351793f1416f2e14bfcf9f465874608a65
SavionShipp <shippsavion209@yahoo.com>
2024-09-17 19:31:15 -0700

    added some css changes

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,12 @@
 export function Header() {
   return (
     <header>
-      <a href="#">Home</a> | <a href="#posts-index">All posts</a> | <a href="#posts-new">New post</a>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#posts-index">All posts</a></li>
+        <li><a href="#posts-new">New post</a></li>
+      </ul>
+      {/* <a href="#">Home</a> | <a href="#posts-index">All posts</a> | <a href="#posts-new">New post</a> */}
     </header>
   );
 }
--- src/PostIndex.jsx
+++ src/PostIndex.jsx
@@ -4,14 +4,15 @@ export function PostIndex(props) {
 
     <div id="posts-index">
       <h1>All posts</h1>
+      <div className="cards">
       {props.posts.map(post => (
         <div key={post.id} className="posts">
-          <h1>{post.title}</h1>
+          <h2>{post.title}</h2>
           <p>{post.body}</p>
           <img src={post.image} alt="" />
         </div>
       ))}
-
+      </div>
       <h2>Savion owns this blog</h2>
       <p>this is all a body paragraph section</p>
       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRyPsC-WOTRffoXvCe-VYnG_97c8b7qavsTA&s" alt="random picture"></img>
