24e5c28e8a0bd552e627f3e7ea8582e975838c1f
SavionShipp <shippsavion209@yahoo.com>
2024-09-19 16:39:14 -0700

    added beginning of modal

--- src/PostIndex.jsx
+++ src/PostIndex.jsx
@@ -13,13 +13,13 @@ export function PostIndex(props) {
         </div>
       ))}
       </div>
-      <h2>Savion owns this blog</h2>
+      {/* <h2>Savion owns this blog</h2>
       <p>this is all a body paragraph section</p>
       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRyPsC-WOTRffoXvCe-VYnG_97c8b7qavsTA&s" alt="random picture"></img>
 
       <h2>This is my last post</h2>
       <p>This is where the paragraph will go for all the information</p>
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSezeZV4X0U6iMxfjDDDZfd6kGr_r91-kGseQ&s" alt="random image"></img>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSezeZV4X0U6iMxfjDDDZfd6kGr_r91-kGseQ&s" alt="random image"></img> */}
 
     </div>
   );
--- src/PostPage.jsx
+++ src/PostPage.jsx
@@ -1,32 +1,26 @@
 import { PostIndex } from "./PostIndex";
 import { PostNew } from "./PostNew";
-
+import axios from "axios"
+import{ useState, useEffect } from 'react';
+import { Modal } from "./Modal";
 export function PostPage() {
-  var posts = [
-    {
-      id: 1,
-      title: "___",
-      body: "___",
-      image: "___",
-    },
-    {
-      id: 2,
-      title: "___",
-      body: "___",
-      image: "___",
-    },
-    {
-      id: 3,
-      title: "___",
-      body: "___",
-      image: "___",
-    },
-  ];
+  const [posts, setPosts] = useState([]);
+
+  const getPostData = () => {
+    axios.get("http://localhost:3000/posts.json").then(response => {
+      console.log(response.data);
+      setPosts(response.data)
+    })
+  }
+
+    useEffect(getPostData, [])
 
   return (
     <>
       <PostNew />
       <PostIndex myname={name} posts={posts} />
+      <Modal />
+      {/* <button onClick={getPostData}>Press for data</button> */}
     </>
   );
 }
